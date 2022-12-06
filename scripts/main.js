7c628d5998592b35a3b3e74d3229a70c87cd0f19
CalmranSyed <68997464+CalmranSyed@users.noreply.github.com>
2022-12-07 01:35:33 +0530

    Remove unwanted css for hero area and header and used Tailwind directives

--- scripts/main.js
+++ scripts/main.js
@@ -1,147 +1,157 @@
 // initialize swiper
 // offers area carousel
-const locationSwiper = new Swiper('.locations-carousel', {
-	spaceBetween: 16,
+const locationSwiper = new Swiper(".locations-carousel", {
+  spaceBetween: 16,
 
-	// Navigation arrows
-	navigation: {
-		nextEl: '.offers-area .swiper-button-next',
-		prevEl: '.offers-area .swiper-button-prev',
-	},
+  // Navigation arrows
+  navigation: {
+    nextEl: ".offers-area .swiper-button-next",
+    prevEl: ".offers-area .swiper-button-prev",
+  },
 
-	breakpoints: {
-		320: {
-			slidesPerView: 1,
-			freeMode: false,
-		},
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      freeMode: false,
+    },
 
-		576: {
-			slidesPerView: 2,
-			freeMode: false,
-		},
-	},
+    576: {
+      slidesPerView: 2,
+      freeMode: false,
+    },
+  },
 });
 
 // explore area carousel
-const exploreLocationSwiper = new Swiper('.explore-carousel', {
-	spaceBetween: 16,
+const exploreLocationSwiper = new Swiper(".explore-carousel", {
+  spaceBetween: 16,
 
-	// Navigation arrows
-	navigation: {
-		nextEl: '.explore-area .swiper-button-next',
-		prevEl: '.explore-area .swiper-button-prev',
-	},
+  // Navigation arrows
+  navigation: {
+    nextEl: ".explore-area .swiper-button-next",
+    prevEl: ".explore-area .swiper-button-prev",
+  },
 
-	breakpoints: {
-		320: {
-			slidesPerView: 1,
-		},
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
 
-		576: {
-			slidesPerView: 3,
-		},
+    576: {
+      slidesPerView: 3,
+    },
 
-		768: {
-			slidesPerView: 4,
-		},
+    768: {
+      slidesPerView: 4,
+    },
 
-		991: {
-			slidesPerView: 5,
-		},
+    991: {
+      slidesPerView: 5,
+    },
 
-		1024: {
-			slidesPerView: 6,
-		},
-	},
+    1024: {
+      slidesPerView: 6,
+    },
+  },
 });
 
 // property type carousel
-const propertyTypeSwiper = new Swiper('.property-carousel', {
-	spaceBetween: 16,
+const propertyTypeSwiper = new Swiper(".property-carousel", {
+  spaceBetween: 16,
 
-	// Navigation arrows
-	navigation: {
-		nextEl: '.property-area .swiper-button-next',
-		prevEl: '.property-area .swiper-button-prev',
-	},
+  // Navigation arrows
+  navigation: {
+    nextEl: ".property-area .swiper-button-next",
+    prevEl: ".property-area .swiper-button-prev",
+  },
 
-	breakpoints: {
-		320: {
-			slidesPerView: 1,
-		},
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
 
-		576: {
-			slidesPerView: 2,
-		},
+    576: {
+      slidesPerView: 2,
+    },
 
-		768: {
-			slidesPerView: 3,
-		},
+    768: {
+      slidesPerView: 3,
+    },
 
-		1024: {
-			slidesPerView: 4,
-		},
-	},
+    1024: {
+      slidesPerView: 4,
+    },
+  },
 });
 
 // trip type carousel
-const tripTypeCarousel = new Swiper('.trip-type-carousel', {
-	spaceBetween: 16,
+const tripTypeCarousel = new Swiper(".trip-type-carousel", {
+  spaceBetween: 16,
 
-	// Navigation arrows
-	navigation: {
-		nextEl: '.trip-type-area .swiper-button-next',
-		prevEl: '.trip-type-area .swiper-button-prev',
-	},
+  // Navigation arrows
+  navigation: {
+    nextEl: ".trip-type-area .swiper-button-next",
+    prevEl: ".trip-type-area .swiper-button-prev",
+  },
 
-	breakpoints: {
-		320: {
-			slidesPerView: 1,
-		},
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
 
-		576: {
-			slidesPerView: 3,
-		},
+    576: {
+      slidesPerView: 3,
+    },
 
-		768: {
-			slidesPerView: 4,
-		},
+    768: {
+      slidesPerView: 4,
+    },
 
-		991: {
-			slidesPerView: 5,
-		},
+    991: {
+      slidesPerView: 5,
+    },
 
-		1024: {
-			slidesPerView: 6,
-		},
-	},
+    1024: {
+      slidesPerView: 6,
+    },
+  },
 });
 
 // property type carousel
-const topRatedSwiper = new Swiper('.top-rated-carousel', {
-	spaceBetween: 16,
+const topRatedSwiper = new Swiper(".top-rated-carousel", {
+  spaceBetween: 16,
 
-	// Navigation arrows
-	navigation: {
-		nextEl: '.top-rated-area .swiper-button-next',
-		prevEl: '.top-rated-area .swiper-button-prev',
-	},
+  // Navigation arrows
+  navigation: {
+    nextEl: ".top-rated-area .swiper-button-next",
+    prevEl: ".top-rated-area .swiper-button-prev",
+  },
 
-	breakpoints: {
-		320: {
-			slidesPerView: 1,
-		},
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
 
-		576: {
-			slidesPerView: 2,
-		},
+    576: {
+      slidesPerView: 2,
+    },
 
-		768: {
-			slidesPerView: 3,
-		},
+    768: {
+      slidesPerView: 3,
+    },
 
-		1024: {
-			slidesPerView: 4,
-		},
-	},
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+const navBtn = $(".nav-btn");
+
+// navigation active element
+
+navBtn.on("click", function (event) {
+  event.preventDefault();
+  $(this).addClass("active");
+  navBtn.not($(this)).removeClass("active");
 });
